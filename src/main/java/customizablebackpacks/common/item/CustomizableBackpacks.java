299b3409557a46bda7e5dccb399b02a67b181e7b
André Steenhoff Madsen <andre@steenhoff.dk>
2021-06-21 15:45:53 +0200

    Added Mekanism Dependency
    Added Runtime only mods (Mekanism/JEI) - THANKS DIRE

---
+++ src/main/java/customizablebackpacks/common/item/CustomizableBackpacks.java
@@ -0,0 +1,15 @@
+package customizablebackpacks.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CustomizableBackpacks.MODID)
+public class CustomizableBackpacks {
+    public static final String MODID = "customizablebackpacks";
+    public static final String MOD_NAME = "Customizable Backpacks";
+    public static final String LOG_TAG = '[' + MOD_NAME + ']';
+
+    // Customizable Backpacks Logger
+    public static final Logger logger = LogManager.getLogger(MOD_NAME);
+}
